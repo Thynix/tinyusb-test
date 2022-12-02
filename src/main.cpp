07e447cf06536fee3814eba1799f57464a020e54
Steve Dougherty <steve@asksteved.com>
2022-12-02 07:27:37 -0500

    Polish and document example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+// Comment out here and in platform.ini to use Arduino Serial
 #include <Adafruit_TinyUSB.h>
 
-void setup() {
-  Serial.begin(9600);
+// This test case is liable to prevent directly uploading code when it doesn't work!
 
+// Double-clicking the reset microswitch will restart it into the
+// bootloader to still allow uploading in such a state.
+
+void setup() {
+  // Turn on builtin LED before Serial.begin()
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
 
+  Serial.begin(9600);
+
+  // Turn off after
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+  // Blink while waiting
   do {
     delay(500);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
     digitalWrite(LED_BUILTIN, LOW);
-  } while (!Serial.availableForWrite());
+  } while (!Serial);
 
   Serial.println("setup done");
 }
